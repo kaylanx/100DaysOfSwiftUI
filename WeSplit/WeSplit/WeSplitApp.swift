61bb2d7aee466eefdd59da9d953a45d4fe0ad54a
Andy Kayley <bitbucket@kayley.name>
2022-03-14 20:24:32 +0000

    Initial Commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andy Kayley on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
