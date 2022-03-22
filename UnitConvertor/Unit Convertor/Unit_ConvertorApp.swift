325286d58ef7deae2d2960988221c768203e5770
Andy Kayley <bitbucket@kayley.name>
2022-03-22 22:04:01 +0000

    Initial Commit

---
+++ UnitConvertor/Unit Convertor/Unit_ConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConvertorApp.swift
+//  Unit Convertor
+//
+//  Created by Andy Kayley on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
