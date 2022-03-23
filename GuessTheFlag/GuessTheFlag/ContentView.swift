452466d59ba53134d61956e709c0f7c6759aa954
Andy Kayley <bitbucket@kayley.name>
2022-03-23 21:25:11 +0000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Andy Kayley on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andy Kayley on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
