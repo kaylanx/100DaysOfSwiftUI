88f98240baefca0a88745c96c5070bfb079b2cee
Andy Kayley <bitbucket@kayley.name>
2022-03-30 21:03:14 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Andy Kayley on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
