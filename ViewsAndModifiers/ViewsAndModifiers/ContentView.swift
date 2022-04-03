a5349ee32dedd7dd3d0400c9d191f6a196e721cc
Andy Kayley <bitbucket@kayley.name>
2022-04-03 12:26:25 +0100

    Binding shizzle

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -64,17 +64,43 @@ extension View {
 }
 
 struct ContentView: View {
+  @State private var agreedToTerms = false
+  @State private var agreedToPrivacyPolicy = false
+  @State private var agreedToEmails = false
   
   var body: some View {
-    VStack {
-      Color.blue
-        .frame(width:200, height: 200)
-        .watermarked(with: "poop")
-      Text("Chicken")
-        .prominentitle()
+    let agreedToAll = Binding<Bool>(
+      get: {
+        agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+      },
+      set: {
+        agreedToTerms = $0
+        agreedToPrivacyPolicy = $0
+        agreedToEmails = $0
+      }
+    )
+    
+    return VStack {
+      Toggle("Agree to terms", isOn: $agreedToTerms)
+      Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+      Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+      Toggle("Agree to all", isOn: agreedToAll)
     }
   }
 }
+
+//struct ContentView: View {
+//  
+//  var body: some View {
+//    VStack {
+//      Color.blue
+//        .frame(width:200, height: 200)
+//        .watermarked(with: "poop")
+//      Text("Chicken")
+//        .prominentitle()
+//    }
+//  }
+//}
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
