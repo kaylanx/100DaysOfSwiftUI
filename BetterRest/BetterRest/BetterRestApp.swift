c677c4a5bb7e81af9486d8dec1b0a098fe725645
Andy Kayley <bitbucket@kayley.name>
2022-04-07 22:16:57 +0100

    Initial Commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andy Kayley on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
