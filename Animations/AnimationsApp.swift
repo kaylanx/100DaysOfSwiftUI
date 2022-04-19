f04ede0c1ae382112c142edb06689602f1c4e186
Andy Kayley <bitbucket@kayley.name>
2022-04-19 21:39:07 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Andy Kayley on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
