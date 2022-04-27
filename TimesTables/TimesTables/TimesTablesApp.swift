68f0befdd2b8777be1eee9affdd2de17baf2a916
Andy Kayley <bitbucket@kayley.name>
2022-04-27 20:33:49 +0100

    Initial Commit

---
+++ TimesTables/TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Andy Kayley on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
