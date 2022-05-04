f9d66241d5030c4ea9036f5cc05e73d39f3c4579
Andy Kayley <bitbucket@kayley.name>
2022-05-04 20:54:20 +0100

    Initial Commit

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Andy Kayley on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
