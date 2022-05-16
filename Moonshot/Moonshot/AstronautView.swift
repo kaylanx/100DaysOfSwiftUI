5d48ffd6ee4f4e92ecf3a08f279f896fad456a21
Andy Kayley <bitbucket@kayley.name>
2022-05-16 21:51:01 +0100

    Day 41

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,39 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Andy Kayley on 16/05/2022.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}
