be25dc26d4b3af7b7978cbcfebada5d82700620b
Andy Kayley <bitbucket@kayley.name>
2022-05-17 20:31:08 +0100

    Initial Commit

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Andy Kayley on 17/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
