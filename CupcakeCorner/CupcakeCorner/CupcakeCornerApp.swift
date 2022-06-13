21fe8b935d6f0a8511a4719422f2a583c4b6784d
Andy Kayley <bitbucket@kayley.name>
2022-06-13 20:27:59 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Andy Kayley on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
