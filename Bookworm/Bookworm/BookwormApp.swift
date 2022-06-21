186ae5a834a09a4cad7860fab7886072d6ba7bec
Andy Kayley <bitbucket@kayley.name>
2022-06-21 21:03:50 +0100

    Initial Commit

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Andy Kayley on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
