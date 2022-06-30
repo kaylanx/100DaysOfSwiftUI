edb7325ab9194ae6ef91bc5c210bf96083301a96
Andy Kayley <bitbucket@kayley.name>
2022-06-30 18:51:11 +0100

    Day 57 Setup

--- CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct CoreDataProjectApp: App {
+    @StateObject private var dataController = DataController()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(
+                    \.managedObjectContext,
+                     dataController.container.viewContext
+                )
         }
     }
 }
---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Andy Kayley on 30/06/2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("CoreData failed to load \(error.localizedDescription)")
+            }
+        }
+    }
+}
