bea4450ecd6e8f858acb2a19c3ed93ae0059ec1f
Andy Kayley <bitbucket@kayley.name>
2022-06-30 18:47:51 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Andy Kayley on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
