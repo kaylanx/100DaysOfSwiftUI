d2c84f6753f4c0f60d9abd669aa59c0042a26a5c
Andy Kayley <bitbucket@kayley.name>
2022-07-12 21:19:58 +0100

    Day 60, Use LazyVStack instead of a list

--- FriendFace/FriendFace/Views/UserDetailView.swift
+++ FriendFace/FriendFace/Views/UserDetailView.swift
@@ -10,74 +10,104 @@ import SwiftUI
 struct UserDetailView: View {
 
     var user: User
-    /*
-     let name: String
-     let age: Int
-     let company: String
-     let email: String
-     let address: String
-     let about: String
-     let registered: Date
-     let tags: [String]
-     let friends: [Friend]
-     */
+
     var body: some View {
-        List {
-            Section(header: VStack(alignment: .center) {
-                Text(user.name)
-                    .font(.largeTitle)
-                Text(user.company)
-                    .font(.body)
-                    .foregroundColor(.gray)
-            }
-            ) {
-                EmptyView()
-            }
-            .headerProminence(.increased)
+        ZStack {
+            Color.systemGroupedBackground
+            ScrollView {
+                LazyVStack {
+
+                            Text(user.name)
+                                .font(.largeTitle)
+                            Text(user.company)
+                                .font(.body)
+                                .foregroundColor(.secondaryLabel)
 
 
-            Section(header: Text("General Info")) {
-                cell(with: "Member since", for: "\(user.formattedRegistered)")
-            }
-            Section {
-                cell(with: "Age", for: "\(user.age)")
-            }
-            Section {
-                cell(with: "Email", for: user.email)
-                cell(with: "Address", for: user.address)
-            }
-            Section {
-                cell(with: "About", for: user.about)
-            }
-            Section(header: Text("Tags")) {
-                ForEach(user.tags, id: \.self) { tag in
-                    Text(tag)
-                }
-            }
-            Section(header: Text("Friends")) {
-                ForEach(user.friends) { friend in
-                    Text(friend.name)
+                    SectionView {
+                        cell(with: "Member since", for: "\(user.formattedRegistered)")
+                    }
+                    SectionView {
+                        cell(with: "Age", for: "\(user.age)")
+                    }
+                    SectionView {
+                        cell(with: "Email", for: user.email)
+                        divider
+                        cell(with: "Address", for: user.address)
+                    }
+                    SectionView {
+                        cell(with: "About", for: user.about)
+                    }
+                    SectionView {
+                        ForEach(user.tags, id: \.self) { tag in
+                            Text(tag)
+                                .padding(.horizontal)
+                                .padding(.top, user.tags.first == tag ? 10 : 0)
+                                .padding(.bottom, user.tags.last == tag ? 10 : 0)
+                            if user.tags.last != tag {
+                                divider
+                            }
+                        }
+                    }
+                    SectionView {
+                        ForEach(user.friends) { friend in
+                            Text(friend.name)
+                            divider
+                        }
+                        .padding()
+                    }
                 }
             }
         }
-        .listStyle(.grouped)
         .navigationBarTitleDisplayMode(.inline)
+        .background(Color.systemGroupedBackground)
+    }
 
+    @ViewBuilder private var divider: some View {
+        Divider()
+            .foregroundColor(.secondaryLabel)
+            .padding(.leading)
     }
 
     func cell(with title: String, for content: String) -> some View {
         VStack(alignment: .leading) {
             Text(title)
                 .font(.caption2)
+                .foregroundColor(.secondaryLabel)
             Text(content)
         }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+    }
+
+}
+
+struct SectionView<Content: View>: View {
+    @ViewBuilder var content: Content
+
+    var body: some View {
+        ZStack {
+            Color.secondarySystemGroupedBackground
+            VStack(alignment: .leading) {
+                content
+            }
+        }
+        .cornerRadius(4)
+        .padding(5)
     }
 }
 
 struct UserDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            UserDetailView(user: PreviewUserRepository.users.first!)
+        Group {
+            NavigationView {
+                UserDetailView(user: PreviewUserRepository.users.first!)
+            }
+
+            NavigationView {
+                UserDetailView(user: PreviewUserRepository.users.first!)
+            }
+            .preferredColorScheme(.dark)
         }
     }
 }
