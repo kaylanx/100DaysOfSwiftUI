8ac35fc19b658433327c851ca88bfcb7fa95b2c8
Andy Kayley <bitbucket@kayley.name>
2022-07-14 21:51:59 +0100

    Day 60 - Remove commented out code

--- FriendFace/FriendFace/ContentView.swift
+++ FriendFace/FriendFace/ContentView.swift
@@ -11,29 +11,6 @@ struct ContentView: View {
 
     var viewModel: UserViewModel
 
-//    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-//
-//    var body: some View {
-//        GeometryReader { fullView in
-//            ScrollView {
-//                ForEach(0..<50) { index in
-//                    GeometryReader { geo in
-//                        Text("Row #\(index)")
-//                            .font(.title)
-//                            .frame(maxWidth: .infinity)
-//                            .background(colors[index % 7])
-//                            .rotation3DEffect(
-//                                .degrees(
-//                                    geo.frame(in: .global).minY - fullView.size.height / 2
-//                                ) / 5,
-//                                axis: (x: 0, y: 1, z: 0)
-//                            )
-//                    }
-//                    .frame(height: 40)
-//                }
-//            }
-//        }
-//    }
     var body: some View {
         NavigationView {
             UserListView(viewModel: viewModel)
