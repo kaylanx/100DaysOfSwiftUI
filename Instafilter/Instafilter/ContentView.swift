0ca7ee72ed88a0423fe154901e4f68dd35917b43
Andy Kayley <bitbucket@kayley.name>
2022-07-20 22:27:01 +0100

    Day 65: Start Instafilter project

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,14 +5,93 @@
 //  Created by Andy Kayley on 17/07/2022.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+
+    @State private var isShowingImagePicker = false
+    @State private var inputImage: UIImage?
+
+    @State private var currentFilter = CIFilter.sepiaTone()
+    private let context = CIContext()
+
     var body: some View {
-//        BluringView()
-//        ConfirmationDialogView()
-//        CoreImageExampleView()
-        UIKitImagePickerView()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    isShowingImagePicker = true
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in
+                            applyProcessing()
+                        }
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+                    Button("Save", action: save)
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .onChange(of: inputImage) { _ in
+                loadImage()
+            }
+            .sheet(isPresented: $isShowingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+        }
+    }
+
+    func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    func changeFilter() {
+
+    }
+
+    func save() {
+
+    }
+
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else {
+            return
+        }
+
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
