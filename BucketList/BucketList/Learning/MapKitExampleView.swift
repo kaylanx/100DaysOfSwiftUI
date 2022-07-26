c55be5965aaa41d4a273d51a192b236fa484ac2c
Andy Kayley <bitbucket@kayley.name>
2022-07-26 21:29:24 +0100

    Day 69: Integrating MapKit with SwiftUI

---
+++ BucketList/BucketList/Learning/MapKitExampleView.swift
@@ -0,0 +1,69 @@
+//
+//  MapKitExampleView.swift
+//  BucketList
+//
+//  Created by Andy Kayley on 26/07/2022.
+//
+
+import MapKit
+import SwiftUI
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct MapKitExampleView: View {
+
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 51.5,
+            longitude: -0.12
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.2
+        )
+    )
+
+    let locations = [
+        Location(
+            name: "Buckingham Palace",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 51.501,
+                longitude: -0.141
+            )
+        ),
+        Location(
+            name: "Tower of London",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 51.508,
+                longitude: -0.076
+            )
+        )
+    ]
+
+    var body: some View {
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
+        }
+    }
+}
+
+struct MapKitExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapKitExampleView()
+    }
+}
