22764adc69ce21dd78b98ef39926bdda5f6ec317
Andy Kayley <bitbucket@kayley.name>
2022-08-03 20:33:42 +0100

    Day 75: Add basic accessibility to Guess The Flag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,11 +11,26 @@ struct FlagImage: View {
 
     let countryName: String
 
+    private let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+
     var body: some View {
         Image(countryName)
             .renderingMode(.original)
             .clipShape(Capsule())
             .shadow(radius: 5)
+            .accessibilityLabel(labels[countryName, default: "Unknown flag"])
     }
 }
 
@@ -24,6 +39,8 @@ struct ContentView: View {
     private let numberOfQuestions = 8
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+
+
     @State private var correctAnswer = Int.random(in: 0...2)
 
     @State private var currentQuestion = 0
@@ -79,6 +96,9 @@ struct ContentView: View {
                             .animation(.easeInOut, value: correctAnswer)
 
                     }
+                    .accessibilityElement()
+                    .accessibilityLabel("\(ContentView.guessTheFlagText) of \(countries[correctAnswer])")
+
                     ForEach(0..<3) { number in
                         Button {
                             withAnimation {
