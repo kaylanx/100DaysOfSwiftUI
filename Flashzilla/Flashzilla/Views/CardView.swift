664c2c5bc9d66430aff8f476a284cb7163a62f4a
Andy Kayley <bitbucket@kayley.name>
2022-09-25 22:10:47 +0100

    Day 89

--- Flashzilla/Flashzilla/Views/CardView.swift
+++ Flashzilla/Flashzilla/Views/CardView.swift
@@ -12,13 +12,27 @@ struct CardView: View {
     let card: Card
     let onRemoved: (() -> Void)?
 
+    @Environment(\.accessibilityDifferentiateWithoutColor) private var differentiateWithoutColor
+
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(
+                    differentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
 
             VStack {
